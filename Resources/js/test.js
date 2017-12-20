3e35f44e0ca1c27db50c2019290fa3e7f09ef153
PeteHass94 <phassard@hotmail.co.uk>
2017-12-20 17:52:11 +0000

    preferred layout

---
+++ Resources/js/test.js
@@ -0,0 +1,23 @@
+
+function displayNav(){
+  document.getElementById("hidden").style.display = "flex";
+}
+function hideNav(){
+  document.getElementById("hidden").style.display = "none";
+}
+
+var isActive = false;
+
+$('.js-menu').on('click', function() {
+	if (isActive) {
+		$(this).removeClass('active');
+		$('body').removeClass('menu-open');
+    $("#menuComment").text("Display Menu");
+	} else {
+		$(this).addClass('active');
+		$('body').addClass('menu-open');
+    $("#menuComment").text("Hide Menu");
+	}
+
+	isActive = !isActive;
+});
